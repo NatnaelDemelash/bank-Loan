0cb17873e73edbe5a44dadd3d5758068efee1861
Natnael Demelash <natidemelash074@gmail.com>
2024-01-10 23:14:05 +0300

    created a redux store

---
+++ src/store.js
@@ -0,0 +1,35 @@
+import { createStore } from "redux";
+
+const initialState = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      return { ...state, loan: action.payload };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+store.dispatch({ type: "account/deposit", payload: 5000 });
+// console.log(store.getState());
+store.dispatch({ type: "account/withdraw", payload: 1000 });
+console.log(store.getState());
